2c8b0ad66a01c744f50906048d159687d9b382fe
TrungHP <htrung1109@gmail.com>
2023-08-15 08:26:13 +0700

    Repair

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using BussinessObject.Models;
+using DataAccess.Infrastructure;
+using Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CapstoneRegistrationContext context= new CapstoneRegistrationContext();
+        IUnitOfWork unitOfWork = new UnitOfWork(context);
+        var student = studentService.GetStudent(2);
+    } 
+     
+}   
